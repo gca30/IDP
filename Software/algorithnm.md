c23dc19302f4b36d54aad83041522aebb57c6673
Cristian Ardeleanu <cbavbz@gmail.com>
2023-11-16 10:56:36 +0000

    changed states to be better

---
+++ Software/algorithnm.md
@@ -0,0 +1,9 @@
+
+# General description of the algorithm
+
+1  2  3  4  5
+6  7  8  9  10
+      13
+
+path: 13 8 3 2
+
--- Software/sketch_nov06a/sketch_nov06a.ino
+++ Software/sketch_nov06a/sketch_nov06a.ino
@@ -9,8 +9,10 @@ const int axleRightLSP = 3;
 const int magneticPin = 999;
 const int ultrasonicPin = A0;
 const int tofPin = 999;
+const int errorLEDPin = 999;
 
 // SENSOR VALUES
+int frontLeft, frontRight, axleLeft,axleRight;
 int button = LOW, prevButton = LOW;
 
 // HARD CODED DISTANCES
@@ -21,13 +23,16 @@ const int rotatetime = 1300;
 char points[] = "x\
 ooooo\
 ooooo\
-xxoxx";
+xooox\
+xoxox";
+
 enum State {
     INACTIVE, // completely stopped
     LINE_FOLLOWING, // we line follow until one of the back sensor is white
     CHANGE_DIRECTION, // we change our direction at a junction until the desiredDir is hit
     DEPOSIT // depositing of the cube from the starting position
 };
+void setState(State newState);
 State state = INACTIVE;
 enum Direction {
     NORTH = -5,
@@ -43,8 +48,9 @@ enum CubeState {
 CubeState cubeState = NO_CUBE;
 int cubesCollected = 0;
 int ourPoint = 13;
-Direction direct = NORTH, desiredDir = NORTH;
-int clearedLine = 0;
+Direction direct = NORTH;
+bool backwards = false;
+Direction desiredDir = NORTH;
 int clearedRotation = 0;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -65,18 +71,26 @@ static inline void setMovement(Movement m) {
         motor2->run(RELEASE);
         break;
       case R_LEFT:
+        motor1->setSpeed(150);
+        motor2->setSpeed(150);
         motor1->run(FORWARD);
         motor2->run(FORWARD);
         break;
       case R_RIGHT:
+        motor1->setSpeed(150);
+        motor2->setSpeed(150);
         motor1->run(BACKWARD);
         motor2->run(BACKWARD);
         break;
       case M_FORWARD:
+        motor1->setSpeed(200);
+        motor2->setSpeed(200);
         motor1->run(FORWARD);
         motor2->run(BACKWARD);
         break;
       case M_BACKWARD:
+        motor1->setSpeed(200);
+        motor2->setSpeed(200);
         motor1->run(BACKWARD);
         motor2->run(FORWARD);
         break;
@@ -96,9 +110,9 @@ void setup() {
         while (1);
     }
     Serial.println("Starting!!!!1!");
-    motor1->setSpeed(250);
+    motor1->setSpeed(150);
     motor1->run(RELEASE);
-    motor2->setSpeed(250);
+    motor2->setSpeed(150);
     motor2->run(RELEASE);
       
     pinMode(frontLeftLSP, INPUT); 
@@ -109,6 +123,58 @@ void setup() {
 }
 
 Direction getDesiredDirection() {
+  if(cubeState == NO_CUBE) {
+    switch(ourPoint) {
+      case 1:
+        return EAST;
+      case 2:
+        if(direct == NORTH)
+          return WEST;
+        else if(direct == EAST)
+          return EAST;
+        break;
+      case 3:
+        return EAST;
+      case 4:
+        if(direct == EAST)
+          return EAST;
+        else if(direct == WEST)
+          return SOUTH;
+        break;
+      case 5:
+        return WEST;
+      case 6:
+        return EAST;
+      case 7:
+        if(direct == WEST)
+          return WEST;
+        else if(direct == EAST)
+          return NORTH;
+        break;
+      case 8:
+        if(direct == NORTH)
+          return WEST;
+        else if(direct == EAST)
+          return EAST;
+        else if(direct == WEST)
+          return NORTH;
+        else if(direct == SOUTH)
+          return SOUTH;
+        break;
+       case 9:
+        if(direct == SOUTH)
+          return EAST;
+        else if(direct == WEST)
+          return WEST;
+        break;
+      case 10:
+        return WEST;
+    }
+  }
+  //digitalWrite(errorLEDPin, HIGH);
+  setState(INACTIVE);
+  return SOUTH;
+  
   // we know ourPoint, current direction, cubeState
   if(cubeState == NO_CUBE) {
     switch(ourPoint) {
@@ -135,11 +201,14 @@ Direction getDesiredDirection() {
   }
 }
 
+bool shouldChangeState = false;
+bool outOfJunction;
+
+void lineFollowingSetup() {
+  outOfJunction = false;
+}
+
 void lineFollowingLoop() {
-    int frontLeft = digitalRead(frontLeftLSP);
-    int frontRight = digitalRead(frontRightLSP);
-    int axleLeft = digitalRead(axleLeftLSP);
-    int axleRight = digitalRead(axleRightLSP);
     
    if (frontLeft == 0 && frontRight == 1)
       setMovement(R_RIGHT);
@@ -150,101 +219,124 @@ void lineFollowingLoop() {
    else if(frontLeft == 0 && frontRight == 0)
       setMovement(M_FORWARD);
 
+    // we can start in a junction and we must first ignore it
     if(axleRight == 0 && axleLeft == 0)
-      clearedLine = false;
-    if(!clearedLine && (axleRight == 1 || axleLeft == 1)) {
-        clearedLine = true;
-        ourPoint += direct;
-    Serial.print("WE ARE AT: ");
-    Serial.print(ourPoint);
-    Serial.print("   ");
-    Serial.println(direct);
-        if(ourPoint == 13) { //arrived at start square
-          state = DEPOSIT;
-        }
-        else {
-          // direct 
-          desiredDir = getDesiredDirection();
-          Serial.print("CHANGE OF DIR: ");
-          Serial.print(direct);
-          Serial.print(" -> ");
-          Serial.println(desiredDir);
-          clearedRotation = false;
-          state = CHANGE_DIRECTION;
-        }
-   }
+      outOfJunction = true;
+    if(outOfJunction && (axleRight == 1 || axleLeft == 1))
+        shouldChangeState = true;
 }
 
-void changeDirectionLoop() {
-    int frontLeft = digitalRead(frontLeftLSP);
-    int frontRight = digitalRead(frontRightLSP);
-    int axleLeft = digitalRead(axleLeftLSP);
-    int axleRight = digitalRead(axleRightLSP);
+int rotationIterations;
 
+void changeDirectionSetup() {
+    desiredDir = getDesiredDirection();
+    outOfJunction = false;
     if(desiredDir == direct) {
-      
-    Serial.print(ourPoint);
-    Serial.print("   ");
-    Serial.println(direct);
-       if(ourPoint == 13)
-          state = DEPOSIT;
-       else
-          state = LINE_FOLLOWING;
-      return;
+      shouldChangeState = true;
+       return;
     }
-
-
-
-    // current direct, desiredDir
+    rotationIterations = direct == -desiredDir ? 2 : 1;
     if(
-      (direct == NORTH && desiredDir == WEST) ||
-      (direct == WEST && desiredDir == SOUTH) ||
-      (direct == SOUTH && desiredDir == EAST) ||
-      (direct == EAST && desiredDir == NORTH)
+        (direct == NORTH && desiredDir == WEST) ||
+        (direct == WEST && desiredDir == SOUTH) ||
+        (direct == SOUTH && desiredDir == EAST) ||
+        (direct == EAST && desiredDir == NORTH)
     )
+      setMovement(R_LEFT);
+    else if(direct == -desiredDir && (ourPoint-1)%5 > 2) // special case when turing at point 6
       setMovement(R_LEFT);
     else
       setMovement(R_RIGHT);
-    
+}
+
+void changeDirectionLoop() {
+
     // it can only exit the loop if it has cleared the line
     if(frontLeft == LOW && frontRight == LOW)
-      clearedRotation = true;
+      outOfJunction = true;
 
     if(frontLeft != HIGH && frontRight != HIGH)
        return; //loop back
-    if(!clearedRotation)
+    if(!outOfJunction)
+        return;
+    if(rotationIterations > 1) {
+      rotationIterations--;
+      outOfJunction = false;  
       return;
-      
-    Serial.println("Stopped rotating");
-
-    setMovement(STOPPED);
-
-    if(desiredDir == -direct) {
-      switch(direct) {
-        case NORTH: direct = EAST;  break;
-        case EAST:  direct = SOUTH; break;
-        case SOUTH: direct = WEST;  break;
-        case WEST:  direct = NORTH; break;
-      }
     }
-    else
-      direct = desiredDir;
+    shouldChangeState = true;
+}
+
+State prevState = LINE_FOLLOWING;
+void setState(State newState) {
+  if(newState == state)
+    return;
+  prevState = state;
+  state = newState;
+  switch(state) {
+    case LINE_FOLLOWING:
+      lineFollowingSetup();
+      break;
+    case CHANGE_DIRECTION:
+      changeDirectionSetup();
+      break;
+    default:
+      break;
+  }
+}
+
+void stateLoop() {
+
+    // Button shuts down
+    if(prevButton == HIGH && button == LOW) {
+        if(state == INACTIVE) {
+            state = prevState;
+            prevState = INACTIVE;
+        }
+        else {
+            prevState = state;
+            state = INACTIVE;
+        }
+    }
+    prevButton = button;
+
+    // Ultrasonic sensor changes LED state
+    // TO DO
+    
+
+    // 
+    if(!shouldChangeState)
+      return;
+    shouldChangeState = false;
+    
+    switch(state) {
+      case LINE_FOLLOWING:
+          ourPoint += direct;
+          if(ourPoint == 13)
+            setState(DEPOSIT);
+          else
+            setState(CHANGE_DIRECTION);
+          break;
+      case CHANGE_DIRECTION:
+         direct = desiredDir;
+         if(ourPoint == 13)
+            setState(DEPOSIT);
+         else
+            setState(LINE_FOLLOWING);
+        break;
+    }
 }
 
 void loop() {
 
     // SENSOR READING
     button = digitalRead(redButtonPin);
+    frontLeft = digitalRead(frontLeftLSP);
+    frontRight = digitalRead(frontRightLSP);
+    axleLeft = digitalRead(axleLeftLSP);
+    axleRight = digitalRead(axleRightLSP);
 
-    // BUTTON
-    if(prevButton == HIGH && button == LOW) {
-       if(state == INACTIVE)
-          state = LINE_FOLLOWING;
-       else
-          state = INACTIVE;
-    }
-    prevButton = button;
-
+    stateLoop();
 
     // ACTION BASED ON STATE
     switch(state) {
@@ -260,5 +352,5 @@ void loop() {
           break;
    }
           
-  delay(10);
+  delay(5);
 }
