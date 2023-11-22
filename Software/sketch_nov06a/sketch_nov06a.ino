d55c3ae131fb365f4f0313c5e15d5e5ac30d278c
tommzhaanghg <49993277+tommzhaanghg@users.noreply.github.com>
2023-11-22 13:00:55 +0000

    cube got

--- Software/sketch_nov06a/sketch_nov06a.ino
+++ Software/sketch_nov06a/sketch_nov06a.ino
@@ -6,8 +6,8 @@
 
 // SENSOR PINS
 const int redButtonPin = 2;
-const int frontLeftLSP = 6; // LSP = line sensor pin
-const int frontRightLSP = 4;
+const int frontLeftLSP = 4; // LSP = line sensor pin
+const int frontRightLSP = 6;
 const int axleLeftLSP = 5;
 const int axleRightLSP = 3;
 const int ultrasonicPin = A0;
@@ -41,7 +41,8 @@ enum State {
     LINE_FOLLOWING, // we line follow until one of the back sensor is white
     CHANGE_DIRECTION, // we change our direction at a junction until the desiredDir is hit
     DEPOSIT, // depositing of the cube from the starting position
-    PICKUP_DELAY // delay when the cube is picked up
+    PICKUP_DELAY, // delay when the cube is picked up
+    VIBE_CHECK
 };
 
 enum Direction {
@@ -53,6 +54,7 @@ enum Direction {
 
 enum CubeState {
     NO_CUBE,
+    CUBE_NEXT_JUNCTION,
     NOT_MAGNETIC,
     MAGNETIC
 };
@@ -83,7 +85,7 @@ Direction directn = NORTH;
 bool backwards = false;
 
 Direction desiredDir = NORTH;
-State prevState = INITIAL;
+State prevState = DEPOSIT;
 int delayCounter = 1000;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -107,14 +109,14 @@ static inline void setMovement(Movement m) {
         case R_LEFT:
             motor1->setSpeed(150);
             motor2->setSpeed(150);
-            motor1->run(BACKWARD);
-            motor2->run(BACKWARD);
+            motor1->run(FORWARD);
+            motor2->run(FORWARD);
             break;
         case R_RIGHT:
             motor1->setSpeed(150);
             motor2->setSpeed(150);
-            motor1->run(FORWARD);
-            motor2->run(FORWARD);
+            motor1->run(BACKWARD);
+            motor2->run(BACKWARD);
             break;
         case M_FORWARD:
             motor1->setSpeed(200);
@@ -163,6 +165,7 @@ void setCubeState(CubeState cb) {
     cubeState = cb;
     switch (cubeState) {
         case NO_CUBE:
+        case CUBE_NEXT_JUNCTION:
             digitalWrite(redLEDPin, LOW);
             digitalWrite(greenLEDPin, LOW);
             break;
@@ -192,26 +195,56 @@ Direction getDesiredDirection() {
         case 2:
             if(directn == WEST)
                 return WEST;
-            else if(
+            else if(directn == EAST)
+                return SOUTH;
             break; 
         case 3:
-            return WEST;
-            
+            if(directn == NORTH) {
+                if(points[10] == 'e') // visited lower right side
+                    return EAST;
+                return WEST;
+            }
+            else if(directn == WEST)
             break; 
         case 4:
+            if(directn == NORTH)
+                return EAST;
+            else if(directn == WEST)
+                return WEST;
             break; 
         case 5:
+            return WEST;
             break; 
         case 6:
+            return EAST;
             break; 
         case 7:
+            if(directn == EAST)
+                return EAST;
+            else if(directn == SOUTH)
+                return WEST;
             break; 
         case 8:
-            return NORTH;
+            if(directn == EAST)
+                return EAST;
+            else if(directn == NORTH) {
+                if(points[6] == 'e') // checked whole right side
+                    return EAST;
+                if(points[1] = 'e' && points[7] != 'e') // checked only upper right side
+                    return WEST;
+                return NORTH;
+            }
+            else if(directn == SOUTH)
+                return SOUTH;
             break; 
         case 9:
+            if(directn == EAST)
+                return EAST;
+            else if(directn == WEST)
+                return NORTH;
             break; 
         case 10:
+            return WEST;
             break; 
         default:
             return directon;
@@ -264,7 +297,7 @@ Direction getDesiredDirection() {
         case 10:
             return WEST;
         case 13:
-            return directn;
+            return NORTH;
         default:
             return directn;
         }
@@ -298,6 +331,8 @@ void updateOnJunction() {
     Serial.print(directn);
     Serial.print(", desired dir = ");
     Serial.print(desiredDir);
+    if(cubeState == CUBE_NEXT_JUNCTION)
+      setCubeState(magnetic ? MAGNETIC : NOT_MAGNETIC);
     if(cubeState != NO_CUBE)
         Serial.print(", with cube");
     Serial.println("");
@@ -337,12 +372,15 @@ void lineFollowingLoop() {
         outOfJunction = true;
     if(outOfJunction && (axleRight == 1 || axleLeft == 1)) {
         updateOnJunction();
-        if(positon == 13 && directn == SOUTH)
+        if(0 < positon + directn && positon + directn < 10 && points[positon + directn] != 'e' && cubeState == NO_CUBE) {
+            setState(VIBE_CHECK);
+            return;
+        }
+        if(positon == 13 && directn == SOUTH) {
             setState(DEPOSIT);
-        else if(desiredDir != directn)
-            setState(CHANGE_DIRECTION);
-        else
-            setState(LINE_FOLLOWING);
+            return;
+        }
+        setState(CHANGE_DIRECTION);
     }
 }
 
@@ -351,6 +389,10 @@ void lineFollowingLoop() {
 int rotationIterations;
 
 void changeDirectionSetup() {
+    if(directn == desiredDir) {
+        setState(LINE_FOLLOWING);
+        return;
+    }
     outOfJunction = false;
     rotationIterations = directn == -desiredDir ? 2 : 1;
     if(
@@ -372,7 +414,7 @@ void changeDirectionLoop() {
     if(frontLeft == LOW && frontRight == LOW)
       outOfJunction = true;
 
-    if(frontLeft != HIGH && frontRight != HIGH)
+    if(frontLeft != HIGH || frontRight != HIGH)
         return; //loop back
     if(!outOfJunction)
         return;
@@ -382,7 +424,34 @@ void changeDirectionLoop() {
         return;
     }
     directn = desiredDir;
-    setState(LINE_FOLLOWING);
+    if(0 < positon + directn && positon + directn < 10 && points[positon + directn] != 'e' && cubeState == NO_CUBE)
+        setState(VIBE_CHECK);
+    else
+        setState(LINE_FOLLOWING);
+}
+
+// VIBE CHECK DELAY
+
+int vibeCheckDelay;
+
+void vibeCheckSetup() {
+    setMovement(STOPPED);
+    vibeCheckDelay = 200;
+}
+
+void vibeCheckLoop() {
+    Serial.println(ultrasonic);
+    if(vibeCheckDelay > 0) {
+        vibeCheckDelay--;
+        return;
+    }
+    if(ultrasonic > 30.0 && ultrasonic < 65.0) {
+        setCubeState(CUBE_NEXT_JUNCTION);
+        Serial.println("CUBE DETECTED HERE");
+    }
+    else
+        points[positon + directn] = 'e';
+    setState(CHANGE_DIRECTION);
 }
 
 // INITIAL STATE
@@ -433,9 +502,9 @@ void inactiveLoop() {
 
 // DEPOSIT STATE
 
-const int depositRotate = 260;
-const int depositForward = 3000;
-const int depositSmallForward = 20;
+const int depositRotate = 300;
+const int depositForward = 1200;
+const int depositSmallForward = 140;
 int depositProgress = 0;
 int depositTimer = 0;
 CubeState initialCubeState = NO_CUBE;
@@ -480,6 +549,9 @@ void depositLoop() {
     depositProgress++;
 }
 
+// VIBE CHECK STATE
+
+
 
 // ----------------
 // STATE MANAGEMENT
@@ -507,8 +579,13 @@ void setState(State newState) {
             break;
         case PICKUP_DELAY:
             pickupDelaySetup();
+            break;
         case INITIAL:
             initialSetup();
+            break;
+        case VIBE_CHECK:
+            vibeCheckSetup();
+            break;
         default:
             break;
     }
@@ -520,17 +597,24 @@ void stateSensors() {
         setState(state == INACTIVE ? prevState : INACTIVE);
 
     // Ultrasonic sensor changes LED state
+    /*
+    if(cubeState == NO_CUBE)
+        Serial.println(ultrasonic);
     if(
         (frontRight == HIGH || frontLeft == HIGH) && 
         state != INACTIVE && state != INITIAL && state != DEPOSIT && cubeState == NO_CUBE &&
-        (ultrasonic < 6.0 || ultrasonic > 480.0)
+        (ultrasonic < 7.0 || ultrasonic > 480.0) && ultrasonic != 0.0
     ) {
-        if(magnetic == HIGH)
+        if(magnetic == HIGH) {
+            Serial.println("MAGNETIC!!");
             setCubeState(MAGNETIC);
-        else
+        }
+        else {
+            Serial.println("NOT MAGNETIC!");
             setCubeState(NOT_MAGNETIC);
+        }
         setState(PICKUP_DELAY);
-    }
+    } */
 }
 
 void statesLoop() {
@@ -553,6 +637,9 @@ void statesLoop() {
             break;
         case CHANGE_DIRECTION:
             changeDirectionLoop();
+            break;
+        case VIBE_CHECK:
+            vibeCheckLoop();
             break;
     }
 }
@@ -599,8 +686,6 @@ void loop() {
     axleRight = digitalRead(axleRightLSP);
     ultrasonic = getUltrasonicDistance();
     magnetic = digitalRead(magneticSensorPin);
-
-    
 
     stateSensors();
     statesLoop();      
